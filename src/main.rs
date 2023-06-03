a07a82f78fe34e7f008e15d0fd3e0843efd7989e
Luis Wirth <lwirth2000@gmail.com>
2023-06-04 01:38:27 +0200

    add screen resolution uniform

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use wgpu::util::DeviceExt;
 use winit::{
   event::{Event, WindowEvent},
   event_loop::{ControlFlow, EventLoop},
@@ -5,6 +6,12 @@ use winit::{
 };
 
 use std::borrow::Cow;
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniform {
+  screen_resolution: [f32; 2],
+}
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
   let size = window.inner_size();
@@ -38,9 +45,43 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
   });
 
+  let mut uniform = Uniform {
+    screen_resolution: [size.width as f32, size.height as f32],
+  };
+
+  let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    label: Some("uniform-buffer"),
+    contents: bytemuck::cast_slice(&[uniform]),
+    usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+  });
+
+  let uniform_bind_group_layout =
+    device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+      label: Some("uniform-bind-group-layout"),
+      entries: &[wgpu::BindGroupLayoutEntry {
+        binding: 0,
+        visibility: wgpu::ShaderStages::FRAGMENT,
+        ty: wgpu::BindingType::Buffer {
+          ty: wgpu::BufferBindingType::Uniform,
+          has_dynamic_offset: false,
+          min_binding_size: None,
+        },
+        count: None,
+      }],
+    });
+
+  let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+    label: Some("uniform-bind-group"),
+    layout: &uniform_bind_group_layout,
+    entries: &[wgpu::BindGroupEntry {
+      binding: 0,
+      resource: uniform_buffer.as_entire_binding(),
+    }],
+  });
+
   let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
     label: None,
-    bind_group_layouts: &[],
+    bind_group_layouts: &[&uniform_bind_group_layout],
     push_constant_ranges: &[],
   });
 
@@ -62,7 +103,6 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     }),
     primitive: wgpu::PrimitiveState {
       topology: wgpu::PrimitiveTopology::TriangleStrip,
-      //strip_index_format: todo!(),
       ..Default::default()
     },
     depth_stencil: None,
@@ -95,6 +135,10 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         config.width = size.width;
         config.height = size.height;
         surface.configure(&device, &config);
+
+        uniform.screen_resolution = [size.width as f32, size.height as f32];
+        queue.write_buffer(&uniform_buffer, 0, bytemuck::cast_slice(&[uniform]));
+
         // On macos the window needs to be redrawn manually after resizing
         window.request_redraw();
       }
@@ -121,6 +165,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             depth_stencil_attachment: None,
           });
           rpass.set_pipeline(&render_pipeline);
+          rpass.set_bind_group(0, &uniform_bind_group, &[]);
           rpass.draw(0..4, 0..1);
         }
 
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,17 +1,25 @@
+struct Uniform {
+  screen_resolution: vec2<f32>,
+}
+
+@group(0) @binding(0) var<uniform> uniform: Uniform;
+
 struct VertexOutput {
   @builtin(position) pos: vec4<f32>,
   @location(0) uv: vec2<f32>,
 };
 
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {
+fn vs_main(
+  @builtin(vertex_index) ivertex: u32
+) -> VertexOutput {
   var positions = array<vec2<f32>, 4>(
     vec2<f32>(-1.0, -1.0),
     vec2<f32>(1.0, -1.0),
     vec2<f32>(-1.0, 1.0),
     vec2<f32>(1.0, 1.0)
   );
-  let pos = positions[in_vertex_index];
+  let pos = positions[ivertex];
 
   var out: VertexOutput;
   out.pos = vec4<f32>(pos, 0.0, 1.0);
@@ -20,6 +28,9 @@ fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {
 }
 
 @fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-  return vec4<f32>(in.uv.x, in.uv.y, 1.0 - in.uv.x, 1.0);
+fn fs_main(
+  in: VertexOutput,
+) -> @location(0) vec4<f32> {
+  var uv = in.pos.xy / uniform.screen_resolution;
+  return vec4<f32>(uv.x, uv.y, 0.0, 1.0);
 }
