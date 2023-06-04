06ad7694f1ffa066672065fafdbfaef8715de909
Luis Wirth <lwirth2000@gmail.com>
2023-06-04 02:58:09 +0200

    implement shader hot reloading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{
+  fs,
+  sync::{Arc, Mutex, OnceLock},
+};
+
+use notify::Watcher;
 use wgpu::util::DeviceExt;
 use winit::{
   event::{Event, WindowEvent},
@@ -5,7 +11,7 @@ use winit::{
   window::Window,
 };
 
-use std::borrow::Cow;
+static SHADER_SRC_PATH: OnceLock<std::path::PathBuf> = OnceLock::new();
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
@@ -14,11 +20,16 @@ struct Uniform {
 }
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
+  SHADER_SRC_PATH
+    .set(std::path::PathBuf::from("./src/shader.wgsl"))
+    .unwrap();
+
   let size = window.inner_size();
 
   let instance = wgpu::Instance::default();
 
   let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
   let adapter = instance
     .request_adapter(&wgpu::RequestAdapterOptions {
       power_preference: wgpu::PowerPreference::default(),
@@ -40,10 +51,18 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     .await
     .expect("Failed to create device");
 
-  let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-    label: None,
-    source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-  });
+  let swapchain_capabilities = surface.get_capabilities(&adapter);
+  let swapchain_format = swapchain_capabilities.formats[0];
+  let mut config = wgpu::SurfaceConfiguration {
+    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+    format: swapchain_format,
+    width: size.width,
+    height: size.height,
+    present_mode: wgpu::PresentMode::Fifo,
+    alpha_mode: swapchain_capabilities.alpha_modes[0],
+    view_formats: vec![],
+  };
+  surface.configure(&device, &config);
 
   let mut uniform = Uniform {
     screen_resolution: [size.width as f32, size.height as f32],
@@ -79,52 +98,70 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     }],
   });
 
-  let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-    label: None,
-    bind_group_layouts: &[&uniform_bind_group_layout],
-    push_constant_ranges: &[],
-  });
+  let device = Arc::new(device);
 
-  let swapchain_capabilities = surface.get_capabilities(&adapter);
-  let swapchain_format = swapchain_capabilities.formats[0];
+  let device_clone = Arc::clone(&device);
 
-  let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-    label: None,
-    layout: Some(&pipeline_layout),
-    vertex: wgpu::VertexState {
-      module: &shader,
-      entry_point: "vs_main",
-      buffers: &[],
-    },
-    fragment: Some(wgpu::FragmentState {
-      module: &shader,
-      entry_point: "fs_main",
-      targets: &[Some(swapchain_format.into())],
-    }),
-    primitive: wgpu::PrimitiveState {
-      topology: wgpu::PrimitiveTopology::TriangleStrip,
-      ..Default::default()
-    },
-    depth_stencil: None,
-    multisample: wgpu::MultisampleState::default(),
-    multiview: None,
-  });
+  let recreate_pipeline = move || -> wgpu::RenderPipeline {
+    let pipeline_layout = device_clone.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+      label: None,
+      bind_group_layouts: &[&uniform_bind_group_layout],
+      push_constant_ranges: &[],
+    });
 
-  let mut config = wgpu::SurfaceConfiguration {
-    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-    format: swapchain_format,
-    width: size.width,
-    height: size.height,
-    present_mode: wgpu::PresentMode::Fifo,
-    alpha_mode: swapchain_capabilities.alpha_modes[0],
-    view_formats: vec![],
+    let shader_source =
+      fs::read_to_string(SHADER_SRC_PATH.get().unwrap()).expect("failed to load shader source");
+
+    let shader = device_clone.create_shader_module(wgpu::ShaderModuleDescriptor {
+      label: None,
+      source: wgpu::ShaderSource::Wgsl(shader_source.into()),
+    });
+
+    device_clone.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: None,
+      layout: Some(&pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: "vs_main",
+        buffers: &[],
+      },
+      fragment: Some(wgpu::FragmentState {
+        module: &shader,
+        entry_point: "fs_main",
+        targets: &[Some(swapchain_format.into())],
+      }),
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleStrip,
+        ..Default::default()
+      },
+      depth_stencil: None,
+      multisample: wgpu::MultisampleState::default(),
+      multiview: None,
+    })
   };
 
-  surface.configure(&device, &config);
+  let render_pipeline = recreate_pipeline();
+  let render_pipeline = Arc::new(Mutex::new(render_pipeline));
+
+  let window = Arc::new(window);
+  let window_clone = Arc::clone(&window);
+
+  let render_pipeline_clone = Arc::clone(&render_pipeline);
+  let mut watcher = notify::recommended_watcher(move |_res| {
+    let mut render_pipeline = render_pipeline_clone.lock().unwrap();
+    *render_pipeline = recreate_pipeline();
+    window_clone.request_redraw();
+  })
+  .unwrap();
+
+  watcher
+    .watch(
+      SHADER_SRC_PATH.get().unwrap(),
+      notify::RecursiveMode::NonRecursive,
+    )
+    .unwrap();
 
   event_loop.run(move |event, _, control_flow| {
-    let _ = (&instance, &adapter, &shader, &pipeline_layout);
-
     *control_flow = ControlFlow::Wait;
     match event {
       Event::WindowEvent {
@@ -149,26 +186,30 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         let view = frame
           .texture
           .create_view(&wgpu::TextureViewDescriptor::default());
+
         let mut encoder =
           device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-        {
-          let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-              view: &view,
-              resolve_target: None,
-              ops: wgpu::Operations {
-                load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                store: true,
-              },
-            })],
-            depth_stencil_attachment: None,
-          });
-          rpass.set_pipeline(&render_pipeline);
-          rpass.set_bind_group(0, &uniform_bind_group, &[]);
-          rpass.draw(0..4, 0..1);
-        }
 
+        let render_pipeline = render_pipeline.lock().unwrap();
+
+        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+          label: None,
+          color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+            view: &view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+              load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+              store: true,
+            },
+          })],
+          depth_stencil_attachment: None,
+        });
+
+        rpass.set_pipeline(&render_pipeline);
+        rpass.set_bind_group(0, &uniform_bind_group, &[]);
+        rpass.draw(0..4, 0..1);
+
+        drop(rpass);
         queue.submit(Some(encoder.finish()));
         frame.present();
       }
